7ba901963534fb0d042a0953fe30e34aeaac80fd
jjinheeWorld <wlsgml450@naver.com>
2022-02-17 01:07:07 +0900

    02_기본 용어

---
+++ javascript-study/javascript-basics/02_기본 용어.js
@@ -0,0 +1,22 @@
+// 코드 구성
+let 안녕 = "하세요";
+let hi = "hello";
+let HI = "HELLO";
+
+console.log(안녕); // 인사 출력
+console.log(hi);  // 소문자 hi
+console.log(HI);  // 대문자 HI
+
+
+// 주석
+// single-line comments
+// print message on the console
+console.log("hello");
+
+console.log("hello~"); // print message on the console
+
+/* multi-line comments
+   print hello message
+   on the console
+*/
+console.log("hello!")
