f576d81b60503b24b1e09a89bf2a25ad6c1c2fe4
jjinheeWorld <wlsgml450@naver.com>
2023-01-27 09:53:38 +0900

    04_연속 부분수열

---
+++ algorithm-study/005_효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/04_연속 부분수열2.js
@@ -0,0 +1,16 @@
+// 연속부분수열의 합이 특정숫자 M이하가 되는 경우가 몇 번 있는지 구하는 프로그램을 작성해보자
+
+function solution(m, arr) {
+  let answer = (lt = sum = 0);
+  for (let rt = 0; rt < arr.length; rt++) {
+    sum += arr[rt];
+    while (sum > m) {
+      sum -= arr[lt++];
+    }
+    answer += rt - lt + 1;
+  }
+  return answer;
+}
+
+let a = [1, 3, 1, 2, 3];
+console.log(solution(5, a));
