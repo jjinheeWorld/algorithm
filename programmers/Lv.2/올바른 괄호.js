8975ace9253c3a1fde28b9c28cc43a2e626b1fee
jjinheeWorld <wlsgml450@naver.com>
2023-09-05 09:47:23 +0900

    Lv.2 올바른 괄호

---
+++ programmers/Lv.2/올바른 괄호.js
@@ -0,0 +1,13 @@
+function solution(s){
+  let answer = true;
+  let stack = []; 
+  for (let x of s) {
+      if (x === '(') stack.push(x);
+      else {
+          if (stack.length === 0) return false;
+          stack.pop();
+      }
+  }
+  if (stack.length > 0) return false;
+  return answer;
+}
