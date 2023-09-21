53821eb9b8f01204df515b564e102dd706fa3ac6
jjinheeWorld <wlsgml450@naver.com>
2023-09-21 15:03:23 +0900

    Lv.1 카드 뭉치

---
+++ programmers/Lv.1/카드 뭉치.js
@@ -0,0 +1,8 @@
+function solution(cards1, cards2, goal) {
+  for (let x of goal) {
+    if (cards1[0] === x) cards1.shift();
+    else if (cards2[0] === x) cards2.shift();
+    else return 'No';
+  }
+  return 'Yes';
+}
