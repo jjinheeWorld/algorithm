a5a031903dc9438ca01e21a628d46516d3732900
jjinheeWorld <wlsgml450@naver.com>
2024-12-02 22:47:26 +0900

    Lv.0 대소문자 바꿔서 출력하기

---
+++ programmers/Lv.0/대소문자 바꿔서 출력하기.js
@@ -0,0 +1,25 @@
+// 영어 알파벳으로 이루어진 문자열 str이 주어집니다.
+// 각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = [line];
+}).on("close", function () {
+  str = input[0].split("");
+  str.forEach((value, index) => {
+    if (value === value.toUpperCase()) {
+      str[index] = value.toLowerCase();
+    } else {
+      str[index] = value.toUpperCase();
+    }
+  });
+
+  console.log(str.join(""));
+});
