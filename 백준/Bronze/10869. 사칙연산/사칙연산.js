934ef0d658321e85a017d16c193df7c833d06193
jinhee lee <88488482+jjinheeWorld@users.noreply.github.com>
2025-03-24 09:29:27 +0900

    [Bronze V] Title: 사칙연산, Time: 100 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split(" ").map(Number);
+const a = input[0];
+const b = input[1];
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(parseInt(a / b));
+console.log(a % b);
