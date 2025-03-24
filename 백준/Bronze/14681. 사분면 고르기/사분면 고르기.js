dbe7a9c8963e58785a08570f780bcf4533666be8
jinhee lee <88488482+jjinheeWorld@users.noreply.github.com>
2025-03-24 12:54:27 +0900

    [Bronze V] Title: 사분면 고르기, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8").toString().trim().split("\n").map(Number);
+const x = input[0];
+const y = input[1];
+
+if (x > 0 && y > 0) console.log(1);
+else if (x < 0 && y > 0) console.log(2);
+else if (x < 0 && y < 0) console.log(3);
+else console.log(4);
