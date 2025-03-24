f7452139d14d1531bc4227ccb55f9ee33a39d15d
jinhee lee <88488482+jjinheeWorld@users.noreply.github.com>
2025-03-24 09:23:40 +0900

    [Bronze V] Title: A/B, Time: 96 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split(" ").map((str) => Number(str));
+const a = input[0];
+const b = input[1];
+console.log(a / b);
