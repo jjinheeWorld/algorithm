e8a3a511cb751b877c733f1f05f6039dd9386e13
jinhee lee <88488482+jjinheeWorld@users.noreply.github.com>
2025-03-26 10:17:17 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 96 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -1,6 +1,4 @@
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = fs.readFileSync("/dev/stdin").toString();
 
-for (let i = 0; i < input.length; i++) {
-  console.log(input[i]);
-}
+console.log(input);
