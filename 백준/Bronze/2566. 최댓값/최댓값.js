e73566819b387c6bde308da85006c2c5380fdf0a
jinhee lee <88488482+jjinheeWorld@users.noreply.github.com>
2025-03-27 09:42:20 +0900

    [Bronze III] Title: 최댓값, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.js
+++ 백준/Bronze/2566. 최댓값/최댓값.js
@@ -1,22 +1,19 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const arr = [];
 let maxValue = Number.MIN_SAFE_INTEGER;
 let maxX = 0;
 let maxY = 0;
 
 for (let i = 0; i < input.length; i++) {
-  arr.push(
-    input[i].split(" ").map((item, j) => {
-      item = Number(item);
-      if (item > maxValue) {
-        maxValue = item;
-        maxX = i + 1;
-        maxY = j + 1;
-      }
-    })
-  );
+  input[i].split(" ").map((item, j) => {
+    item = Number(item);
+    if (item > maxValue) {
+      maxValue = item;
+      maxX = i + 1;
+      maxY = j + 1;
+    }
+  });
 }
 
 console.log(maxValue);
