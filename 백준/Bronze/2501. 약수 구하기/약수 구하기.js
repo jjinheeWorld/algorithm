b450e8e349a8ce829e2a47e486a1e4a08d91795f
jinhee lee <88488482+jjinheeWorld@users.noreply.github.com>
2025-03-28 09:34:01 +0900

    [Bronze III] Title: 약수 구하기, Time: 112 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const [n, k] = fs.readFileSync("/dev/stdin").toString().trim().split(" ").map(Number);
+
+let arr = [];
+
+for (let i = 1; i <= n; i++) {
+  if (n % i === 0) {
+    arr.push(i);
+  }
+}
+
+console.log(arr[k - 1] || 0);
