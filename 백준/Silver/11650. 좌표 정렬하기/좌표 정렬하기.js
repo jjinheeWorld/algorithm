27a5e800e92310885ff24f593250a95a967fece7
jinhee lee <88488482+jjinheeWorld@users.noreply.github.com>
2025-04-03 10:37:48 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 3732 ms, Memory: 62700 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -5,13 +5,8 @@ const n = +input[0];
 const arr = input.splice(1).map((str) => str.split(" ").map(Number));
 
 arr.sort((a, b) => {
-  if (a[0] < b[0]) return -1;
-  else if (a[0] > b[0]) return 1;
-  else {
-    if (a[1] < b[1]) return -1;
-    else if (a[1] > a[1]) return 1;
-    else return 0;
-  }
+  if (a[0] === b[0]) return a[1] - b[1];
+  else return a[0] - b[0];
 });
 
 arr.forEach((v) => console.log(...v));
