1617acf258db90a8d072ca3df3707a607ccba1aa
jinhee lee <88488482+jjinheeWorld@users.noreply.github.com>
2025-04-04 09:53:28 +0900

    [Silver V] Title: 단어 정렬, Time: 216 ms, Memory: 20116 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const n = +input[0];
+const words = input.splice(1);
+const uniqueWords = [...new Set(words)];
+
+uniqueWords.sort((a, b) => {
+  if (a.length !== b.length) return a.length - b.length;
+  return a.localeCompare(b);
+});
+
+console.log(uniqueWords.join("\n"));
