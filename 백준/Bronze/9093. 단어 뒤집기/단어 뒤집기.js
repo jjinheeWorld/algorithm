ed41e7b7ad3e21b4d1ffe2059b772f93b36f5327
jinhee lee <88488482+jjinheeWorld@users.noreply.github.com>
2025-04-07 09:57:21 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 576 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.js
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.js
@@ -2,11 +2,10 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const n = Number(input[0]);
-const arr = input.splice(1);
 
-for (let str of arr) {
-  const answer = str
+for (let i = 1; i <= n; i++) {
+  const reversedWords = input[i]
     .split(" ")
     .map((word) => word.split("").reverse().join(""));
-  console.log(answer.join(" "));
+  console.log(reversedWords.join(" "));
 }
