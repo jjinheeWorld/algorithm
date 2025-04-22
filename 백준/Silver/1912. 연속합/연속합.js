7dfa6469b9ec876197620a23d8cd093bf2e29ad9
jinhee lee <88488482+jjinheeWorld@users.noreply.github.com>
2025-04-22 10:41:57 +0900

    [Silver II] Title: 연속합, Time: 168 ms, Memory: 14412 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.js
+++ 백준/Silver/1912. 연속합/연속합.js
@@ -4,11 +4,12 @@ const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const n = +input[0];
 const arr = input[1].split(" ").map(Number);
 
-const dp = Array(n).fill(0);
-dp[0] = arr[0];
+let maxSum = arr[0];
+let currentSum = arr[0];
 
 for (let i = 1; i < n; i++) {
-  dp[i] = Math.max(arr[i], dp[i - 1] + arr[i]);
+  currentSum = Math.max(arr[i], currentSum + arr[i]);
+  maxSum = Math.max(maxSum, currentSum);
 }
 
-console.log(Math.max(...dp));
+console.log(maxSum);
