ba6378123dde7e15ce1c5b343607647b6e14b1d1
jinhee lee <88488482+jjinheeWorld@users.noreply.github.com>
2025-05-01 09:39:19 +0900

    [Gold V] Title: 암호 만들기, Time: 148 ms, Memory: 10020 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.js
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.js
@@ -2,16 +2,16 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const [l, c] = input[0].split(" ").map(Number);
-const arr = input[1].split(" ").sort();
+const chars = input[1].split(" ").sort();
 
-const vowel = ["a", "e", "i", "o", "u"];
+const vowels = new Set(["a", "e", "i", "o", "u"]);
 
 const selected = [];
 const answer = [];
 
 const backtracking = (start, depth) => {
   if (depth === l) {
-    const vowelCount = selected.filter((al) => vowel.includes(al)).length;
+    const vowelCount = selected.filter((al) => vowels.has(al)).length;
     const consonantCount = selected.length - vowelCount;
 
     if (vowelCount >= 1 && consonantCount >= 2) {
@@ -21,7 +21,7 @@ const backtracking = (start, depth) => {
   }
 
   for (let i = start; i < c; i++) {
-    selected[depth] = arr[i];
+    selected[depth] = chars[i];
     backtracking(i + 1, depth + 1);
   }
 };
