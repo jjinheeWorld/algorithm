1faa54d56215c4b071f60704dc41f8fcfadf8ce2
jjinheeWorld <wlsgml450@naver.com>
2025-06-02 21:55:52 +0900

    [level 1] Title: 과일 장수 리팩토링

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,16 +1,9 @@
 function solution(k, m, score) {
-    const answer = [];
-    score.sort((a, b) => b - a);
-    for (let i = 0; i < score.length; i += m) {
-        const box = [];
-        for (let j = i; j < i + m; j++) {
-            if (!score[j]) break;
-            box.push(score[j]);
-        }
-        if (box.length === m) {
-            const minScore = Math.min(...box);
-            answer.push(minScore * m);
-        }
-    }
-    return answer.reduce((a, b) => a + b, 0);
+  const answer = [];
+  score.sort((a, b) => b - a);
+  for (let i = 0; i + m <= score.length; i += m) {
+    const minScore = score[i + m - 1];
+    answer.push(minScore * m);
+  }
+  return answer.reduce((a, b) => a + b, 0);
 }
