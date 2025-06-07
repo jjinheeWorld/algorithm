936d82c011d14d1cc2c108972309d0d1aa239b6d
jinhee lee <88488482+jjinheeWorld@users.noreply.github.com>
2025-06-07 18:34:27 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 4.18 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,7 @@
+function solution(t, p) {
+    let count = 0;
+    for (let i = 0; i <= t.length - p.length; i++) {
+        if (Number(p) >= Number(t.slice(i, i + p.length))) count++;
+    }
+    return count;
+}
