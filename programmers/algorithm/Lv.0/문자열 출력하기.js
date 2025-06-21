bdccba44637a5e46cf49c8699542c73f9649347a
jjinheeWorld <wlsgml450@naver.com>
2025-06-21 16:21:07 +0900

    remove: remove folders

--- programmers/algorithm/Lv.0/문자열 출력하기.js
+++
@@ -1,9 +0,0 @@
-// 문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
-
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.on("line", console.log);
