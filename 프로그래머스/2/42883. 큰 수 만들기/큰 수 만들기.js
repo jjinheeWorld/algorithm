826a4da43443752228d122f7829b0406df61c260
jinhee lee <88488482+jjinheeWorld@users.noreply.github.com>
2025-11-03 10:20:32 +0900

    [level 2] Title: 큰 수 만들기, Time: 59.57 ms, Memory: 57.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -0,0 +1,12 @@
+function solution(number, k) {
+    const stack = [];
+    for (let i = 0; i < number.length; i++) {
+        while (k > 0 && stack.length && stack[stack.length - 1] < number[i]) {
+            stack.pop();
+            k--;
+        }
+        stack.push(number[i]);
+    }
+    
+    return stack.slice(0, stack.length - k).join("");
+}
