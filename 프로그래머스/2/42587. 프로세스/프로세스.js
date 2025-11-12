bfe1025f15d5ba92acd91c3e48e7c88a8dfe4bbe
jinhee lee <88488482+jjinheeWorld@users.noreply.github.com>
2025-11-12 11:46:19 +0900

    [level 2] Title: 프로세스, Time: 20.49 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,13 +1,13 @@
 function solution(priorities, location) {
     const queue = priorities.map((priority, index) => [priority, index]);
     let order = 0;
+    
     while (queue.length > 0) {
         const current = queue.shift();
-        if (queue.some(([priority, _]) => priority > current[0])) queue.push(current);
+        if (queue.some(([priority, _]) => current[0] < priority)) queue.push(current);
         else {
             order++;
             if (current[1] === location) return order;
         }
     }
-
 }
