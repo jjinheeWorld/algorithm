e4e2c3bceee3dbf16df4a36a5f369eb47d4d8695
jinhee lee <88488482+jjinheeWorld@users.noreply.github.com>
2026-02-04 11:44:51 +0900

    [level 3] Title: 네트워크, Time: 0.45 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -4,16 +4,18 @@ function solution(n, computers) {
     
     const dfs = (node) => {
         visited[node] = true;
+        
         for (let next = 0; next < n; next++) {
-            if (node === next) continue;
-            if (computers[node][next] === 1 && !visited[next]) dfs(next);
+            if (computers[node][next] === 1 && !visited[next]) {
+                dfs(next);
+            }
         }
     }
     
     for (let i = 0; i < n; i++) {
         if (!visited[i]) {
-            dfs(i);
             count++;
+            dfs(i);
         }
     }
     
