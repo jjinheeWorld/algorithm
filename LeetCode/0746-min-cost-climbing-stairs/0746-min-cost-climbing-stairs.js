f62485e994fd0d06cf3759281317f205bb061b01
jinhee lee <wlsgml450@naver.com>
2026-02-12 17:59:42 +0900

    Time: 3 ms (29.37%), Space: 57.1 MB (14.64%) - LeetHub

---
+++ LeetCode/0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+const minCostClimbingStairs = function(cost) {
+    const n = cost.length;
+    const dp = Array(n).fill(-1);
+    dp[0] = 0;
+    dp[1] = 0;
+
+    for (let i = 2; i < n + 1; i++) {
+        dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+    }
+
+    return dp[n];
+};
