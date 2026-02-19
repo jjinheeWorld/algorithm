a745a9f25eac3e30eb5b45a4e8d7e4f600385562
jinhee lee <wlsgml450@naver.com>
2026-02-19 23:38:17 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 22.74 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,15 @@
+function solution(a, b) {
+    let sum = 0;
+    
+    if (a > b) {
+        let c = a;
+        a = b;
+        b = c;
+    }
+    
+    for (let i = a; i <= b; i++) {
+        sum += i;    
+    }
+    
+    return sum;
+}
