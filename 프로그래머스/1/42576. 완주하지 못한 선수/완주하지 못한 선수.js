2e1b820b509e0474f9b3fb62a724f183f83b5d0f
jinhee lee <wlsgml450@naver.com>
2026-03-02 11:01:10 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.13 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,12 +1,15 @@
 function solution(participant, completion) {
     const players = new Map();
-    participant.forEach(player => {
-        if (players.has(player)) players.set(player, players.get(player) + 1);
-        else players.set(player, 1);
-    });
-    completion.forEach(player => {
+    
+    for (const player of participant) {
+        if (!players.has(player)) players.set(player, 1);
+        else players.set(player, players.get(player) + 1);
+    }
+    
+    for (const player of completion) {
         if (players.get(player) > 1) players.set(player, players.get(player) - 1);
         else players.delete(player);
-    });
+    }
+    
     return players.keys().next().value;
 }
