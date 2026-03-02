717bc1105088e6f94354fed761b2a9c51bbbefb6
jinhee lee <wlsgml450@naver.com>
2026-03-02 09:23:58 +0900

    [level 2] Title: 피보나치 수, Time: 4.51 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    const dp = [];
+    dp[0] = 0;
+    dp[1] = 1;
+    
+    for (let i = 2; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+    }
+    
+    return dp[n];
+}
