a5d6dc93f8eabdcfc03d30b5dc6b132e34c18808
jinhee lee <wlsgml450@naver.com>
2026-03-09 20:20:33 +0900

    [level 2] Title: 전화번호 목록, Time: 471.12 ms, Memory: 99.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,13 +1,13 @@
 function solution(phone_book) {
     const phoneSet = new Set(phone_book);
-    for (const number of phone_book) {
+    
+    for (const phone of phone_book) {
         let prefix = "";
-        for (const char of number) {
+        for (const char of phone) {
             prefix += char;
-            if (prefix !== number && phoneSet.has(prefix)) {
-                return false;
-            }
+            if (phone !== prefix && phoneSet.has(prefix)) return false;
         }
     }
+    
     return true;
 }
