6d325295c465d2fc77708fb9df557df9ce65b991
jinhee lee <wlsgml450@naver.com>
2026-03-10 23:55:30 +0900

    [Silver III] Title: 두 수의 합, Time: 184 ms, Memory: 18492 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.js
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.js
@@ -1,24 +1,24 @@
 const fs = require("fs");
-const input = fs
-  .readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const n = Number(input[0]);
 const arr = input[1].split(" ").map(Number);
 const x = Number(input[2]);
 
 arr.sort((a, b) => a - b);
+let left = 0;
+let right = n - 1;
 let count = 0;
 
-for (let left = 0; left < n; left++) {
-  let right = n - 1;
-  while (left < right) {
-    let sum = arr[left] + arr[right];
-    if (sum === x) {
-      count++;
-      break;
-    } else {
-      right--;
-    }
+while (left < right) {
+  const sum = arr[left] + arr[right];
+  if (sum === x) {
+    count++;
+    left++;
+  } else if (sum > x) {
+    right--;
+  } else {
+    left++;
   }
 }
 
