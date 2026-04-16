ae7476b1e9c21097b36865e8a4df1e35a7b4d755
jinhee lee <wlsgml450@naver.com>
2026-04-16 18:38:03 +0900

    [level 2] Title: 기능개발, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,16 +1,22 @@
 function solution(progresses, speeds) {
     const answer = [];
-    const days = progresses.map((progress, i) => Math.ceil((100 - progress) / speeds[i]));
+    const days = progresses.map((progress, index) => {
+        return Math.ceil((100 - progress) / speeds[index]);
+    });
+    
     let flag = days[0];
     let count = 0;
-    for (let i = 0; i < days.length; i++) {
-        if (flag >= days[i]) count++;
-        else {
+    
+    for (const day of days) {
+        if (flag >= day) {
+            count++;
+        } else {
             answer.push(count);
             count = 1;
-            flag = days[i];
+            flag = day;
         }
     }
+    
     answer.push(count);
     return answer;
 }
