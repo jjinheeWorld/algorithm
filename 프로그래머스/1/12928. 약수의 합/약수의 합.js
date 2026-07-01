14e9a23b98a1406f5f32998c8d4f9a177a5c9c36
jinhee lee <wlsgml450@naver.com>
2026-07-01 17:05:21 +0900

    [level 1] Title: 약수의 합, Time: 0.15 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,11 +1,11 @@
 function solution(n) {
-    let answer = 0;
+    let sum = 0;
     
-    if (n === 0) return answer;
+    if (n === 0) return sum;
     
     for (let i = 1; i <= n; i++) {
-        if (n % i === 0) answer += i;
+        if (n % i === 0) sum += i;
     }
     
-    return answer;
+    return sum;
 }
