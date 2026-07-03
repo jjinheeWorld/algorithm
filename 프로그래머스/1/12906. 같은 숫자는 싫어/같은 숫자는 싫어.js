7db4b98d82fbe0f5ee17004f03b5a33f456e5a36
jinhee lee <wlsgml450@naver.com>
2026-07-03 09:55:13 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 33.96 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,7 +1,9 @@
 function solution(arr) {
     const stack = [];
+    
     for (const number of arr) {
-        if (stack[stack.length - 1] !== number) stack.push(number); 
+        if (stack[stack.length - 1] !== number) stack.push(number);
     }
+    
     return stack;
 }
